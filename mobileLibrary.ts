f5eb2cab993623b0417413932f9b2ebb83b99bdd
TaniaSanchezM <tania.sm90@gmail.com>
2021-10-16 18:46:23 +0200

    class mobileLibrary

---
+++ mobileLibrary.ts
@@ -0,0 +1,42 @@
+import { Mobile } from './classMobile';
+export class MobileLibrary{
+    private name:string;
+    private location:string;
+    private mobiles: Mobile[]
+    private totalPrice: number
+
+    constructor(name:string, location:string, mobiles:Mobile[]){
+        this.name = name;
+        this.location = location;
+        this.mobiles = mobiles;
+    }
+
+// métodos públicos (getters)
+public getName():string{
+    return this.name
+}
+public getLocation():string{
+    return this.location
+}
+public getMobiles():Mobile[]{
+    return this.mobiles
+}
+// Métodos públicos (setters)
+public setName(name:string):void{
+    this.name = name
+}
+public setLocation(location:string):void{
+    this.location = location
+}
+public setMobiles(mobiles:Mobile[]):void{
+    this.mobiles = mobiles
+}
+// Otros métodos públicos
+public totalPriceCalculator(){
+    let sumaPrecio:number = 0
+    for (let i = 0; i < this.mobiles.length; i++){
+        sumaPrecio += this.mobiles[i].getPrice();
+    }
+    sumaPrecio = this.totalPrice
+} 
+}
---
+++ mobileLibraryTest.ts
@@ -0,0 +1,15 @@
+import { Mobile } from './classMobile';
+import { MobileLibrary } from './mobileLibrary';
+
+let telefono1 = new Mobile ("Nokia3210", "3210", "Nokia", 8, "negro", false, 1, 99);
+let telefono2 = new Mobile ("iPhone3G", "3G", "iPhone", 32, "blanco", false, 2, 149);
+let telefono3 = new Mobile ("Samsung Galaxy 10", "Galaxy 10", "Samsung", 128, "azul", true, 4, 499);
+let telefono4 = new Mobile ("Xiaomi Mi10A", "Mi10A", "Xiaomi", 128, "rojo", true, 5, 359);
+
+let myArrMobile = [telefono1, telefono2, telefono3, telefono4];
+
+let miLibreria = new MobileLibrary ("Mi libreria", "Estanteria izquierda", myArrMobile);
+
+console.log(miLibreria.getName());
+console.log(miLibreria.getLocation());
+console.log(miLibreria.getMobiles());
